92f86f8563d7b6a9d48c42369adf155ae8c716b0
Andrei Barabas <andrei.barabas@argentumapp.com>
2019-03-24 00:59:52 +0200

    50% of iOS done

--- ios/RNGetSocial.m
+++ ios/RNGetSocial.m
@@ -1,4 +1,5 @@
 #import "RNGetSocial.h"
+#import <React/RCTConvert.h>
 #import <React/RCTLog.h>
 #import <GetSocial/GetSocial.h>
 #import <GetSocialUI/GetSocialUI.h>
@@ -16,36 +17,90 @@
 
 RCT_EXPORT_MODULE()
 
-+(GetSocial *)getSocial
+- (NSDictionary *)constantsToExport
 {
-    @synchronized(self) {
-        static GetSocial *instance;
-        static dispatch_once_t once = 0;
-        dispatch_once(&once, ^{
-           // TODO implement
-        });
-        return instance;
-    }
+    return @{
+             
+             // add the LinkParams constants
+             @"KEY_CUSTOM_TITLE": GetSocial_Custom_Title,
+             @"KEY_CUSTOM_DESCRIPTION": GetSocial_Custom_Description,
+             @"KEY_CUSTOM_IMAGE": GetSocial_Custom_Image,
+             @"KEY_CUSTOM_YOUTUBE_VIDEO": GetSocial_Custom_YouTubeVideo,
+             
+             // use "constants" on the module to account for the scenario where the events
+             // have been issued
+             // before the js has been loaded
+             @"initialized": @([GetSocial isInitialized]),
+             @"userId": [GetSocialUser userId]
+             };
 }
+
 
 + (BOOL)requiresMainQueueSetup {
-    return YES;
+    return NO;  //run on background thread
 }
 
 
-+ (void)initGetSocial:(NSString *)appId {
+//
+// whenInitialized Promise
+//
+RCT_REMAP_METHOD(whenInitialized,
+                 whenInitializedWithResolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+
     [GetSocial executeWhenInitialized:^() {
-        RCTLogInfo(@"Get Social SDK initialized");
-        // TODO implement
+        // GetSocial is ready to be used
+
+        // finalize the promise
+        resolve(nil);
     }];
-    [GetSocial initWithAppId:appId];
 }
 
-// TODO implement show invite
+
+
+
+//
+// getReferralData Promise
+//
+RCT_REMAP_METHOD(getReferralData,
+                 getReferralDataWithResolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+    
+    [GetSocial referralDataWithSuccess:^(GetSocialReferralData *_Nullable referralData) {
+       
+        if (referralData == nil)
+        {
+            // finalize the promise with No details
+            resolve(nil);
+        }
+        else
+        {
+            resolve([referralData referralLinkParams]);
+        }
+    }
+     
+    failure:^(NSError *_Nonnull error) {
+        
+        // something went wrong
+        reject(@"GetSocial", @"Failed getting referral data", error);
+    }];
+}
+
+//
+// showInviteUI Promise
+//
 RCT_REMAP_METHOD(showInviteUI,
-                  showInviteUIWithResolver:(RCTPromiseResolveBlock)resolve
+                  showInviteUIWithParams: (NSDictionary *)params
+                  config: (NSDictionary *)config
+                  resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
 {
+    
+    //run the invite on the UI thread
+    dispatch_async(dispatch_get_main_queue(), ^{
+        
     BOOL wasShown = [[GetSocialUI createInvitesView] show];
     if (wasShown) {
         NSLog(@"GetSocial Smart Invites UI was shown");
@@ -56,6 +111,7 @@ RCT_REMAP_METHOD(showInviteUI,
         NSError *error = [NSError errorWithDomain:@"You FAILED!" code:200 userInfo:details];
         reject(@"no_ui", @"There were no invite UI shown - ", error);
     }
+    });
 }
 
 @end
